b6dea0aa2546c2d9315f77467114ccc87e5a225f
JCluzet <jo@cluzet.fr>
2022-11-04 12:20:26 +0100

    basic start with react and login page-background+visual identity

---
+++ front/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import LoginPage from './LoginPage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoginPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
