7eddfda445c02eca1ba5d4d3f5f8ff73c2378488
Jo Cluzet <jo@jos-mac-studio.home>
2022-11-30 19:25:06 +0100

    better assembly

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import logo from "../assets/images/logo.svg";
 import schoollogo from "../assets/images/schoollogo.png";
+import Footer from "../components/Footer";
 import ParticleBackground from "../particlesBackground/ParticleBackground";
+
 
 export default function Login() {
     // state
     const HandleLogin = () => {
         const uid = "u-s4t2ud-b8671e1262fdfa3d6496559c9ff540b7fc6b2f04a49b80dc17edb3ea42e2638b";
-        window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=" + uid + "&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fapi_callback&response_type=code";
+        window.location.href = "https://api.intra.42.fr/oauth/authorize?client_id=" + uid + "&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code";
         
         // if connexion ok => redirect to dashboard
         // else => error message
@@ -31,12 +33,7 @@ export default function Login() {
           </div>
         </div>
       </div>
-        <footer>
-            <div className="footer">
-                Created with <span className="heart">❤</span> by <a href="https://github.com/jcluzet">jcluzet</a> , <a href="https://github.com/jessy-damoiseau">jessy-damoiseau</a>, <a href="https://github.com/draak-z">draak-z</a>, <a href="https://github.com/misterwayne">misterwayne</a> & <a href="https://github.com/tkomaris">tkomaris</a> 
-
-            </div>
-        </footer>
+        <Footer/>
     </div>
   );
 }
