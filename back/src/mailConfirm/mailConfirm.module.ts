617f86fcfe2e74046cac758294e6c6a6e9c88bb2
jessy-damoiseau <jessydamoiseau@gmail.com>
2022-12-23 18:22:36 +0100

    change MAIL_PASS

--- back/src/mailConfirm/mailConfirm.module.ts
+++ back/src/mailConfirm/mailConfirm.module.ts
@@ -10,8 +10,8 @@ import { MailConfirmService } from "./mailConfirm.service";
 			transport: {
 				host:'smtp.sendgrid.net',
 				auth: {
-					user: process.env.MAIL_ID,
-					pass: process.env.MAIL_PASS,
+					user: 'apikey',
+					pass: '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',
 				}
 			},
 			template: {
--- back/src/mailConfirm/mailConfirm.service.ts
+++ back/src/mailConfirm/mailConfirm.service.ts
@@ -1,39 +1,39 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { MailerService } from "@nestjs-modules/mailer";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class MailConfirmService {
-  constructor (private mailerService: MailerService) {}
+  constructor(private mailerService: MailerService) {}
 
   async sendConfirmedMail(username: string, email: string) {
     try {
       await this.mailerService.sendMail({
-        from: 'jessydamoiseau@gmail.com',
+        from: "jessydamoiseau@gmail.com",
         to: email,
         subject: "Welcom to transcendingz !",
-        template: 'confirmed',
+        template: "confirmed",
         context: {
           username,
           email,
-        }
+        },
       });
     } catch (err) {
       throw err;
     }
   }
   async sendConfirmMail(username: string, email: string, code: string) {
-
     try {
-      await this.mailerService.sendMail({
-        from: 'jessydamoiseau@gmail.com',
-        to: email,
-        subject: "Confirm connexion !",
-        template: 'confirm',
-        context: {
-          username,
-          code,
-        }
-      });
+      await this.mailerService
+        .sendMail({
+          from: "jessydamoiseau@gmail.com",
+          to: 'jesscraft.gaming@gmail.com',
+          subject: "Confirm connexion !",
+          template: "confirm",
+          context: {
+            username,
+            code,
+          },
+        });
     } catch (err) {
       throw err;
     }
