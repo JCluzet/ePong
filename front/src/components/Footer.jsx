577618eb1fd1f229125878aceafa67c7c6a1ce5f
Tatiana Komaristaia <74381051+tkomaris@users.noreply.github.com>
2023-01-07 20:45:38 +0100

    Update Footer.jsx

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -12,7 +12,6 @@ export default function Footer() {
         <a href="https://github.com/jcluzet">jcluzet</a> ,{" "}
         <a href="https://github.com/jessy-damoiseau">jessy-damoiseau</a>,{" "}
         <a href="https://github.com/draak-z">draak-z</a>,{" "}
-        <a href="https://github.com/misterwayne">misterwayne</a> &{" "}
         <a href="https://github.com/tkomaris">tkomaris</a>
       </div>
     </footer>
