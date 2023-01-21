9db09ce42aa01859df2d6b9a7980632480f89c94
JCluzet <jo@cluzet.fr>
2023-01-21 18:31:08 +0100

    fix credit in footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
       <div className="footer">
         Created with <span className="heart">❤</span> by{" "}
         <a href="https://github.com/jcluzet">jcluzet</a> ,{" "}
-        <a href="https://github.com/jessy-damoiseau">jessy-damoiseau</a>,{" "}
-        <a href="https://github.com/draak-z">draak-z</a>,{" "}
-        <a href="https://github.com/Misterwayne">mwane</a>,{" "}
-        <a href="https://github.com/Vigorate">ambelkac</a>
+        <a href="https://github.com/jessy-damoiseau">jdamoise</a>,{" "}
+        <a href="https://github.com/Vigorate">ambelkac</a>, {" "}
+        <a href="https://github.com/draak-z">bmaudet</a>,{" "}
+        <a href="https://github.com/Misterwayne">mwane</a>
       </div>
     </footer>
   );
