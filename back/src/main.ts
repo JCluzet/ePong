0ec0e8eb9a764b246965cbf8c756ab1fb0e44898
JCluzet <jo@cluzet.fr>
2023-01-30 00:20:13 +0100

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 const PORT = process.env.PORT || 5001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cors({ origin: '*' }));
   await app.listen(PORT);
 }
 bootstrap();
